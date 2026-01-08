25e0bc1e2a83dc368dc3098d74f72386eeb8aa0d
robin <robin.martin67@ynov.com>
2026-01-08 15:08:09 +0100

    Enhance index page and server logic with dynamic form handling and environment variable display

--- main.go
+++ main.go
@@ -1,25 +1,53 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
+type PageData struct {
+	EnvMessage string
+	FormResult string
+}
+
 func main() {
 	port := os.Getenv("PORT")
-
 	if port == "" {
 		port = "8080"
 	}
 
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
+	tmpl := template.Must(template.ParseFiles("static/index.html"))
+
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		message := os.Getenv("MESSAGE_ACCUEIL")
+		if message == "" {
+			message = "Aucune variable 'MESSAGE_ACCUEIL' détectée."
+		}
+
+		data := PageData{
+			EnvMessage: message,
+		}
+		tmpl.Execute(w, data)
+	})
+
+	http.HandleFunc("GET /hello/{name}", func(w http.ResponseWriter, r *http.Request) {
+		name := r.PathValue("name")
+		w.Write([]byte("Bonjour " + name + " ! (Ceci est une route dynamique)"))
+	})
+
+	http.HandleFunc("POST /submit", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		nomUtilisateur := r.FormValue("mon_champ")
+
+		data := PageData{
+			EnvMessage: os.Getenv("MESSAGE_ACCUEIL"),
+			FormResult: "Reçu côté serveur : " + nomUtilisateur,
+		}
+		tmpl.Execute(w, data)
+	})
 
 	log.Printf("Serveur lancé sur le port %s", port)
-
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
